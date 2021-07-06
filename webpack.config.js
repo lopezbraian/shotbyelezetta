6e914e0d817c3524bcc910435f633d547d0dd3aa
Braian Lopez <braian.lopez.ze@outlook.com>
2021-07-06 01:52:12 -0300

    🏗️

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.png|.jpg|.mp4/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {},
+          },
+          {
+            loader: "sass-loader",
+            options: {},
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name]-styles.css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+};
