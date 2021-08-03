005e3980ed88f18b55dc893305db777f7d208629
Braian Emanuel Lopez <braian.lopez.ze@outlook.com>
2021-08-03 18:53:17 -0300

    done respponsive

--- src/page/About/index.js
+++ src/page/About/index.js
@@ -7,21 +7,12 @@ import premier from '../../assets/logo/premier.png'
 import lightroom from '../../assets/logo/lightroom.png'
 import sony from '../../assets/logo/sony.png'
 
-import './index.css'
+import './index.scss'
 
 export default function About() {
   return (
-    <div>
-      <section className="group-video">
-        <h2 className="title-section">Producciones Audiovisuales</h2>
-        <p className="text-section">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
-          esse dolorum illum magnam, velit unde animi. Libero expedita natus
-          voluptatem velit. Quae magnam ut dolorum. Sint unde nobis ullam ab.
-        </p>
-      </section>
-
-      <section className="quality">
+    <section className="about-container">
+      <div className="container-text">
         <h2 className="title-section">
           Usando las mejores tecnologías del mercado
         </h2>
@@ -29,34 +20,34 @@ export default function About() {
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
           esse dolorum illum magnam, velit unde animi. Libero expedita natus
         </p>
-        <div className="quality-container">
-          <div className="quality-container__video">
-            <video src={video} loop width="100%" autoPlay muted></video>
+      </div>
+      <div className="quality-container">
+        <div className="quality-container__video">
+          <video src={video} loop width="100%" autoPlay muted></video>
+        </div>
+        <div className="quality-container__tecnologias">
+          <div className="box-tec">
+            <img width="100px" height="100px" src={photo} alt="" />
+            <p>Adobe Photoshop</p>
           </div>
-          <div className="quality-container__tecnologias">
-            <div className="box-tec">
-              <img width="100px" height="100px" src={photo} alt="" />
-              <p>Adobe Photoshop</p>
-            </div>
-            <div className="box-tec">
-              <img width="100px" height="100px" src={illus} alt="" />
-              <p>Adobe Illustrator</p>
-            </div>
-            <div className="box-tec">
-              <img width="100px" height="100px" src={lightroom} alt="" />
-              <p>Adobe Lightroom</p>
-            </div>
-            <div className="box-tec">
-              <img width="100px" height="100px" src={premier} alt="" />
-              <p>Adobe Premiere</p>
-            </div>
-            <div className="box-tec">
-              <img width="100px" height="100px" src={sony} alt="" />
-              <p>Sony A7 III</p>
-            </div>
+          <div className="box-tec">
+            <img width="100px" height="100px" src={illus} alt="" />
+            <p>Adobe Illustrator</p>
+          </div>
+          <div className="box-tec">
+            <img width="100px" height="100px" src={lightroom} alt="" />
+            <p>Adobe Lightroom</p>
+          </div>
+          <div className="box-tec">
+            <img width="100px" height="100px" src={premier} alt="" />
+            <p>Adobe Premiere</p>
+          </div>
+          <div className="box-tec">
+            <img width="100px" height="100px" src={sony} alt="" />
+            <p>Sony A7 III</p>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   )
 }
--- src/page/Video/index.js
+++ src/page/Video/index.js
@@ -5,19 +5,22 @@ import TextSection from '../../components/TextSection'
 
 export default function Video({ data }) {
   return (
-    <section>
-      <TitleSection text={'Producciones Videoclips'} />
-      <TextSection
-        text={`
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
-        esse dolorum illum magnam, velit unde animi. Libero expedita natus
-        voluptatem velit. Quae magnam ut dolorum. Sint unde nobis ullam ab.
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur
-        minus consectetur rem a dolorem vero deleniti repudiandae eaque corrupti
-        perferendis mollitia sit, ducimus animi optio culpa, similique assumenda
-        suscipit quos?
-      `}
-      />
+    <section className="page_video-container">
+      <div className="container-text">
+
+        <TitleSection text={'Producciones Videoclips'} />
+        <TextSection
+          text={`
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
+          esse dolorum illum magnam, velit unde animi. Libero expedita natus
+          voluptatem velit. Quae magnam ut dolorum. Sint unde nobis ullam ab.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur
+          minus consectetur rem a dolorem vero deleniti repudiandae eaque corrupti
+          perferendis mollitia sit, ducimus animi optio culpa, similique assumenda
+          suscipit quos?
+        `}
+        />
+      </div>
       <div className="group-video__container">
         <div className="video_item">
           <iframe
